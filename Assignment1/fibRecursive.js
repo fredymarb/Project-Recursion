5dd3be802b455cccedf66e6e7bd8ea13f98942f6
Fredrick Marbell <29468132+fredymarb@users.noreply.github.com>
2024-02-23 09:04:09 +0000

    finished fib with recursion

---
+++ Assignment1/fibRecursive.js
@@ -0,0 +1,24 @@
+function fibonacci(n) {
+    // base case
+    if (n < 2) {
+        return n;
+    }
+
+    // recursive call
+    return fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+function fib(n, counter = 0, arr = []) {
+    // base case
+    if (arr.length >= n) {
+        return arr;
+    }
+
+    // add num to arr on every recursive call
+    arr.push(fibonacci(counter));
+
+    // recursive call
+    return fib(n, counter += 1, arr);
+}
+
+console.log(fib(15));
